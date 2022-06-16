48a8d795417260c14bae24a3601257268be67cd1
dgozhenko <daniil.gozhenko@gmail.com>
2022-06-16 16:55:58 +0300

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Daniil Gozhenko on 16.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
