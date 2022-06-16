1b4e617f09b7bb2aa409747aa4cb3bc96e7f5675
dgozhenko <daniil.gozhenko@gmail.com>
2022-06-16 22:28:19 +0300

    Connect screen to app

--- Crypto/Core/CryptoApp.swift
+++ Crypto/Core/CryptoApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct CryptoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                CryptoExchangeView()
+            }
         }
     }
 }
